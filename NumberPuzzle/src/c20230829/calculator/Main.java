3f62bb1836beb25ced30b5bc061202a83d599915
KDS01 <rlagnek12@naver.com>
2023-08-29 17:59:34 +0900

    calc and interface test

--- NumberPuzzle/src/c20230829/calculator/Main.java
+++ NumberPuzzle/src/c20230829/calculator/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main extends Calculator {
 	public static void main(String[] args) {
 		Calculator calc=new Calculator();
-		calc.calculate("/", 1,3,4,5);
+		calc.calculate("+", 1,3,4,5);
 		
 	}
 }
